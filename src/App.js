8480b4aa81941bf0ccc7ad81eb79c30007c2c40e
George Webster <george.webster@nationwide.co.uk>
2021-02-15 21:54:19 +0000

    initial commit - basic post and resposne handled

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [authenticated, setAuthenticated] = useState();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        onClick={() =>
+          axios
+            .get("http://localhost:8080/")
+            .then((res) => console.log("response from request: ", res.data))
+        }
+      >
+        Test GET
+      </button>
+      <form>
+        <label htmlFor="email">Email</label>
+        <input
+          type="text"
+          name="email"
+          placeholder="email address"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </form>
+      <button
+        onClick={() => {
+          axios.post("http://localhost:8080/", { user: email, password }).then(
+            (res) => {
+              console.log("response from server is: ", res.data);
+              setAuthenticated(res.data.authenticated);
+            },
+            (err) => console.error("error in post call: ", err)
+            // why can't error be displayed?
+          );
+        }}
+      >
+        Test POST
+      </button>
+      <h1>You {authenticated ? "are" : "are not"} authenticated</h1>
+      {authenticated && (
+        <button onClick={() => console.log("secrets")}>
+          Continue on with your journey
+        </button>
+      )}
     </div>
   );
 }
