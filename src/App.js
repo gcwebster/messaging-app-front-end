7bb49aaf4a02e4874a499b5f81c94a28998eee75
George Webster <george.webster@nationwide.co.uk>
2021-05-11 21:34:29 +0100

    app.js cleanup

--- src/App.js
+++ src/App.js
@@ -10,57 +10,62 @@ import {
   decreaseCounter,
 } from "./redux/Counter/counter.actions";
 
-import { saveMessages } from "./redux/Messages/message.actions";
+import { saveMessages, setUser } from "./redux/Messages/message.actions";
 
 function App(props) {
   const history = useHistory();
   const [authenticated, setAuthenticated] = useState();
   const [user, setUser] = useState();
+  const [showRedux, setShowRedux] = useState();
 
   useEffect(() => {
-    axios.get(`http://localhost:8080/messages/${user}`).then((res) => {
-      console.log("user is: ", user);
-      props.saveMessages(res?.data?.messages);
-      history.push("/messages");
-    });
+    if (user !== undefined) {
+      axios.get(`http://localhost:8080/messages?user=${user}`).then((res) => {
+        console.log("user is: ", user);
+        props.saveMessages(res?.data?.messages);
+        props.setUser(user);
+        if (authenticated) history.push("/messages");
+      });
+    }
   }, [user]);
 
   return (
     <>
-      <h1>
-        Why do I have to allow POST in CORS (express index.js) but not GET?
-      </h1>
-      {!authenticated && (
-        <SignIn setAuthenticated={setAuthenticated} setUser={setUser} />
+      <SignIn setAuthenticated={setAuthenticated} setUser={setUser} />
+
+      <button onClick={() => setShowRedux(!showRedux)}>
+        {showRedux ? "Hide " : "Show "} Redux counter?
+      </button>
+      {showRedux && (
+        <>
+          <div>Count: {props.count}</div>
+
+          <button onClick={() => props.increaseCounter()}>
+            Increase Count
+          </button>
+
+          <button onClick={() => props.decreaseCounter()}>
+            Decrease Count
+          </button>
+        </>
       )}
-      <div className="App">
-        <div>Count: {props.count}</div>
-
-        <button onClick={() => props.increaseCounter()}>Increase Count</button>
-
-        <button onClick={() => props.decreaseCounter()}>Decrease Count</button>
-      </div>
-
-      <button onClick={() => props.saveMessages()}>Save Messages</button>
     </>
   );
 }
 
-const mapStateToProps = (state) => {
-  return {
-    count: state.counter.count,
-    messages: state.messages.messages,
-  };
-};
+const mapStateToProps = (state) => ({
+  count: state.counter.count,
+  messages: state.messages.messages,
+});
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    increaseCounter: () => dispatch(increaseCounter()),
+const mapDispatchToProps = (dispatch) => ({
+  increaseCounter: () => dispatch(increaseCounter()),
 
-    decreaseCounter: () => dispatch(decreaseCounter()),
+  decreaseCounter: () => dispatch(decreaseCounter()),
 
-    saveMessages: (payload) => dispatch(saveMessages(payload)),
-  };
-};
+  saveMessages: (payload) => dispatch(saveMessages(payload)),
+
+  setUser: (payload) => dispatch(setUser(payload)),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
