d16aa10a6ad29e357443a8271b5d88c75bb7dae4
George Webster <george.webster@nationwide.co.uk>
2021-08-25 08:54:35 +0100

    changed Input to link label with <input />

--- src/common/Input/index.jsx
+++ src/common/Input/index.jsx
@@ -3,7 +3,7 @@ import "./input.css";
 
 const Input = ({ label, type, name, placeholder, onChange, value }) => (
   <div className="input">
-    <label htmlFor={label}>{label}</label>
+    <label htmlFor={name}>{label}</label>
     <input
       type={type}
       name={name}
